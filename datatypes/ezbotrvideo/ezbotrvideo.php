547c9435b7efaad8bdb1a95b8fc8bff0d347a1d0
David Broadfoot <david@thinkcreative.com>
2012-04-30 15:30:34 -0400

    take into account empty id

--- datatypes/ezbotrvideo/ezbotrvideo.php
+++ datatypes/ezbotrvideo/ezbotrvideo.php
@@ -20,53 +20,59 @@ class eZBotrVideo
 			}
 		}
 		
-		$response1 = $botr_api->call('/videos/show', array('video_key'=>$id));
-		$response2 = $botr_api->call('/videos/conversions/list', array('video_key'=>$id));
-
-		if ($response1['video']['status'] != 'ready') {
-			
-			$this->Attributes = array('conversion' => '', 'html'=>"<p>You're video has been uploaded. Conversion details will appear shortly.</p>", 'video_key' => $id, 'response' => array($response1, $response2), 'download' => '/content/error/404');
-			
+		if ($id == '') {
+			$this->Attributes = array('conversion' => '', 'html'=>"<p>No video currently loaded.</p>", 'video_key' => $id, 'response' => array(false, false), 'download' => '/content/error/404');
 		} else {
-
-			$pa = $si = $du = $di = array();
 		
-			$vid = '/content/error/404';
+			$response1 = $botr_api->call('/videos/show', array('video_key'=>$id));
+			$response2 = $botr_api->call('/videos/conversions/list', array('video_key'=>$id));
 
-			foreach ($response2['conversions'] as $c) {
-				$tmp = $c['link']['protocol'].$c['link']['address'].$c['link']['path'];
-				$pa[]=$tmp;
-				if (strpos($tmp,'videos') !== false && $vid == '/content/error/404') $vid = $tmp;
-				$si[]=$c['filesize'];
-				$du[]=$c['duration'];
-				$di[]=$c['width'] . "x" . $c['height'];
-				$tmp = $c['key'];
-				$se[]="<input type='radio' name='vidselect' value='$tmp'/>";
+			if ($response1['video']['status'] != 'ready') {
+			
+				$this->Attributes = array('conversion' => '', 'html'=>"<p>Your video has been uploaded. Conversion details will appear shortly.</p>", 'video_key' => $id, 'response' => array($response1, $response2), 'download' => '/content/error/404');
+			
+			} else {
+
+				$pa = $si = $du = $di = array();
+		
+				$vid = '/content/error/404';
+
+				foreach ($response2['conversions'] as $c) {
+					$tmp = $c['link']['protocol'].$c['link']['address'].$c['link']['path'];
+					$pa[]=$tmp;
+					if (strpos($tmp,'videos') !== false && $vid == '/content/error/404') $vid = $tmp;
+					$si[]=$c['filesize'];
+					$du[]=$c['duration'];
+					$di[]=$c['width'] . "x" . $c['height'];
+					$tmp = $c['key'];
+					$se[]="<input type='radio' name='vidselect' value='$tmp'/>";
+				}
+
+				$pr = "<img src='http://cdn.thinkcreative.com/thumbs/" . $id . "-100.jpg'/>";
+				$pa = implode($pa, '<br/>');
+				$si = implode($si, '<br/>');
+				$du = implode($du, '<br/>');
+				$di = implode($di, '<br/>');
+				$se = implode($se, '<br/>');
+
+				$html = "<table class='list' cellspacing='0'><tr><th class='tight'>Preview</th><th>Path</th><th>Size</th><th>Duration</th><th>Dimensions</th></tr><tr><td>$pr</td><td>$pa</td><td>$si</td><td>$du</td><td>$di</td></tr></table>";
+
+				$upath = preg_replace("/-[^\.]*\./", ".", $vid);
+
+				$path = explode("/", $upath);
+				$path = 'videos/' . array_pop($path);
+
+				$path = preg_replace("/-[^\.]*\./", ".", $path);
+		
+		
+				$expires = time() + 3600;
+				$signature = md5($path.':'.$expires.':'.$Private);
+
+				$downloadme  =  $upath.'?exp='.$expires.'&sig='.$signature;
+		
+				$this->Attributes = array('download' => $downloadme, 'html' => $html, 'response' => array($response1, $response2), 'args' => $botr_api->getargs());
+		
 			}
-
-			$pr = "<img src='http://cdn.thinkcreative.com/thumbs/" . $id . "-100.jpg'/>";
-			$pa = implode($pa, '<br/>');
-			$si = implode($si, '<br/>');
-			$du = implode($du, '<br/>');
-			$di = implode($di, '<br/>');
-			$se = implode($se, '<br/>');
-
-			$html = "<table class='list' cellspacing='0'><tr><th class='tight'>Preview</th><th>Path</th><th>Size</th><th>Duration</th><th>Dimensions</th></tr><tr><td>$pr</td><td>$pa</td><td>$si</td><td>$du</td><td>$di</td></tr></table>";
-
-			$upath = preg_replace("/-[^\.]*\./", ".", $vid);
-
-			$path = explode("/", $upath);
-			$path = 'videos/' . array_pop($path);
-
-			$path = preg_replace("/-[^\.]*\./", ".", $path);
-		
-		
-			$expires = time() + 3600;
-			$signature = md5($path.':'.$expires.':'.$Private);
-
-			$downloadme  =  $upath.'?exp='.$expires.'&sig='.$signature;
-		
-			$this->Attributes = array('download' => $downloadme, 'html' => $html, 'response' => array($response1, $response2), 'args' => $botr_api->getargs());
 		
 		}
 		
