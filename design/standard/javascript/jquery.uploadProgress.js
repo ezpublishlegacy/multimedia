f3c3f923d89a63ab73ed873e516aae2c9bcf756d
David Broadfoot <david@thinkcreative.com>
2013-08-08 13:51:16 -0400

    fixes for jquery 1.9

--- design/standard/javascript/jquery.uploadProgress.js
+++ design/standard/javascript/jquery.uploadProgress.js
@@ -1,125 +1,128 @@
-if (/WebKit/i.test(navigator.userAgent) && top.document == document) { // sniff
-  var _timer = setInterval(function() {
-    if (/loaded|complete/.test(document.readyState)) {
-      clearInterval(_timer);
-	  iframe = document.createElement('iframe');
-	  iframe.name = "progressFrame";
-	  iframe.style.width = 0;
-	  iframe.style.height = 0;
-	  iframe.style.position = 'absolute';
-	  iframe.style.top = '-3000px';	  
-	  document.body.appendChild(iframe);
+function runYourFunctionWhenJQueryIsLoaded() {
+    if (window.jQuery){
+        //possibly some other JQuery checks to make sure that everything is loaded here
 
-	  var d = iframe.contentWindow.document;
-	  d.open();
-	  /* weird - safari won't load scripts without this lines... */
-	  d.write('<html><head></head><body></body></html>');
-	  d.close();
-
-	  var b = d.body;
-
-	  var s = d.createElement('script');
-	  s.src = "/extension/multimedia/design/multimedia/javascript/jquery-1.4.2.min.js";
-	  b.appendChild(s);
-	  var s1 = d.createElement('script');
-	  s1.src = "/extension/multimedia/design/standard/javascript/jquery.uploadProgress.js";
-	  b.appendChild(s1);
+        yourFunctionToRun();
+    } else {
+        setTimeout(runYourFunctionWhenJQueryIsLoaded, 50);
     }
-  }, 10);
 }
 
-/*
-* jquery.uploadProgress
-*
-* Copyright (c) 2008 Piotr Sarnacki (drogomir.com)
-*
-* Licensed under the MIT license:
-* http://www.opensource.org/licenses/mit-license.php
-*
-* Source:
-* http://github.com/drogus/jquery-upload-progress/blob/a518b05fa85b1833bb3a8e8c2a0c8dfb9d6abaf1/jquery.uploadProgress.js
-*
-* 2009-11-05 Sergey Lashin (www.bitsontherun.com)
-* - Updated to work with Bits on the Run upload server
-*/
-(function($) {
-  $.fn.uploadProgress = function(options) {
-  options = $.extend({
-    dataType: "jsonp",
-    interval: 2000,
-    progressBar: "#progressbar",
-    progressUrl: "/progress",
-    uploadToken: "",
-    start: function() {},
-    uploading: function() {},
-    complete: function() {},
-    success: function() {},
-    error: function() {},
-    preloadImages: [],
-    jqueryPath: '/extension/ezjscore/design/standard/javascript/jquery-1.4.2.min.js',
-    uploadProgressPath: '/extension/multimedia/design/standard/javascript/jquery.uploadProgress.js',
-    timer: ""
-  }, options);
+runYourFunctionWhenJQueryIsLoaded();
 
-  $(function() {
-    //preload images
-    for(var i = 0; i<options.preloadImages.length; i++)
-    {
-     options.preloadImages[i] = $("<img>").attr("src", options.preloadImages[i]);
-    }
-  });
+function yourFunctionToRun(){
+	
+	jQuery(function() {
+		if(navigator.userAgent.toLowerCase().indexOf('safari') > 0 && navigator.userAgent.toLowerCase().indexOf('chrome') == -1 && top.document == document) {
+		  /* iframe to send ajax requests in safari
+		   thanks to Michele Finotto for idea */
+		  iframe = document.createElement('iframe');
+		  iframe.name = "progressFrame";
+		  jQuery(iframe).css({width: '0', height: '0', position: 'absolute', top: '-3000px'});
+		  document.body.appendChild(iframe);
 
-  return this.each(function(){
-    $(this).bind('submit', function() {
-      /* start callback */
-      options.start();
+		  var d = iframe.contentWindow.document;
+		  d.open();
+		  /* weird - safari won't load scripts without this lines... */
+		  d.write('<html><head><script type="text/javascript" src="/extension/multimedia/design/standard/iframe_assets/jquery-1.4.2.min.js"></script><script type="text/javascript" src="/extension/multimedia/design/standard/javascript/jquery.uploadProgress.js"></script></head><body></body></html>');
+		  d.close();
+		}
+	});
 
-      var uploadProgress = (navigator.userAgent.indexOf('Safari') > 0) ? progressFrame.jQuery.uploadProgress : jQuery.uploadProgress;
-      options.timer = window.setInterval(function() { uploadProgress(this, options) }, options.interval);
-    });
-  });
-  };
+	/*
+	* jquery.uploadProgress
+	*
+	* Copyright (c) 2008 Piotr Sarnacki (drogomir.com)
+	*
+	* Licensed under the MIT license:
+	* http://www.opensource.org/licenses/mit-license.php
+	*
+	* Source:
+	* http://github.com/drogus/jquery-upload-progress/blob/a518b05fa85b1833bb3a8e8c2a0c8dfb9d6abaf1/jquery.uploadProgress.js
+	*
+	* 2009-11-05 Sergey Lashin (www.bitsontherun.com)
+	* - Updated to work with Bits on the Run upload server
+	*/
+	(function(jQuery) {
+	  jQuery.fn.uploadProgress = function(options) {
+	  options = jQuery.extend({
+	    dataType: "jsonp",
+	    interval: 2000,
+	    progressBar: "#progressbar",
+	    progressUrl: "/progress",
+	    uploadToken: "",
+	    start: function() {},
+	    uploading: function() {},
+	    complete: function() {},
+	    success: function() {},
+	    error: function() {},
+	    preloadImages: [],
+	    jqueryPath: '/extension/multimedia/design/standard/iframe_assets/jquery-1.4.2.min.js',
+	    uploadProgressPath: '/extension/multimedia/design/standard/javascript/jquery.uploadProgress.js',
+	    timer: ""
+	  }, options);
 
-jQuery.uploadProgress = function(e, options) {
-  jQuery.ajax({
-    type: "GET",
-    url: options.progressUrl,
-    data: "token=" + options.uploadToken,
-    dataType: options.dataType,
-    success: function(upload) {
-	  proc = 0;
-      if (upload.state == 'uploading') {
-        upload.percents = Math.floor((upload.received / upload.size)*1000)/10;
+	  jQuery(function() {
+	    //preload images
+	    for(var i = 0; i<options.preloadImages.length; i++)
+	    {
+	     options.preloadImages[i] = jQuery("<img>").attr("src", options.preloadImages[i]);
+	    }
+	  });
 
-        var barN = $.browser.safari ? $(options.progressBar, parent.document) : $(options.progressBar);
-        barN.css({width: upload.percents+'%'});
-        options.uploading(upload);
-		proc=1;
-      }
+	  return this.each(function(){
+	    jQuery(this).bind('submit', function() {
+	      /* start callback */
+	      options.start();
 
-      if (upload.state == 'done' || upload.state == 'error') {
-        window.clearInterval(options.timer);
-        options.complete(upload);
-		proc=1;
-      }
+	      var uploadProgress = navigator.userAgent.toLowerCase().indexOf('safari') > 0 && navigator.userAgent.toLowerCase().indexOf('chrome') == -1 ? progressFrame.jQuery.uploadProgress : jQuery.uploadProgress;
+	      options.timer = window.setInterval(function() { uploadProgress(this, options) }, options.interval);
+	    });
+	  });
+	  };
 
-      if (upload.state == 'done') {
-        options.success(upload);
-		proc=1;
-      }
+	jQuery.uploadProgress = function(e, options) {
+	  jQuery.ajax({
+	    type: "GET",
+	    url: options.progressUrl,
+	    data: "token=" + options.uploadToken,
+	    dataType: options.dataType,
+	    success: function(upload) {
+		  proc = 0;
+	      if (upload.state == 'uploading') {
+	        upload.percents = Math.floor((upload.received / upload.size)*1000)/10;
 
-      if (upload.state == 'error') {
-        options.error(upload);
-		proc=1;
-      }
-	  
-	  if (upload == '' || upload.state == 'starting') {
-		proc=1;
-      }
-	  
-	  if (proc==0) alert("Sorry - An unknown error occured! Please discard this ez draft and try again!");
-    }
-  });
-};
+	        var barN = navigator.userAgent.toLowerCase().indexOf('safari') > 0 && navigator.userAgent.toLowerCase().indexOf('chrome') == -1 ? jQuery(options.progressBar, parent.document) : jQuery(options.progressBar);
+	        barN.css({width: upload.percents+'%'});
+	        options.uploading(upload);
+			proc=1;
+	      }
 
-})(jQuery);
+	      if (upload.state == 'done' || upload.state == 'error') {
+	        window.clearInterval(options.timer);
+	        options.complete(upload);
+			proc=1;
+	      }
+
+	      if (upload.state == 'done') {
+	        options.success(upload);
+			proc=1;
+	      }
+
+	      if (upload.state == 'error') {
+	        options.error(upload);
+			proc=1;
+	      }
+
+		  if (upload == '' || upload.state == 'starting') {
+			proc=1;
+	      }
+
+		  if (proc==0) alert("Sorry - An unknown error occured! Please discard this ez draft and try again!");
+	    }
+	  });
+	};
+
+	})(jQuery);
+	
+}
--- design/standard/javascript/jquery.urlEncode.js
+++ design/standard/javascript/jquery.urlEncode.js
@@ -1,4 +1,4 @@
-jQuery.extend({URLEncode:function(c){var o='';var x=0;c=c.toString();var r=/(^[a-zA-Z0-9_.]*)/;
+$.extend({URLEncode:function(c){var o='';var x=0;c=c.toString();var r=/(^[a-zA-Z0-9_.]*)/;
   while(x<c.length){var m=r.exec(c.substr(x));
     if(m!=null && m.length>1 && m[1]!=''){o+=m[1];x+=m[1].length;
     }else{if(c[x]==' ')o+='+';else{var d=c.charCodeAt(x);var h=d.toString(16);
